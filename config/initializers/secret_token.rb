84ae73498c8cb70ed9fae898661a3e9e80509970
Eliza Brock <eliza@elizabrocksoftware.com>
2011-12-17 10:56:32 -0600

    Initial generated rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Migrainetracker::Application.config.secret_token = '3057930101f27af2e2c62430f6de59572508ea4daf6521641cb46f9f0f4e678c6aff676311c7989a8b6393f221374347e1c47c556e209c973492ccc3b1226a75'
