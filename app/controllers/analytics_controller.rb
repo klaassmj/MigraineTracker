dd18c27dd19de1a6ca03adbb49c5a0141ce381ad
Jackson <jackson@jackson-HP-EliteBook-8530w.(none)>
2012-05-10 17:34:47 -0400

    edited the analytics controller to allow functionality for day analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,7 +1,11 @@
  class AnalyticsController < ApplicationController
   def index
-    
+     respond_to do |format|
+       format.html
+     end
   end 
+
+
   def viewMigraineTimeAnalytics
     
 
@@ -29,4 +33,29 @@
     
   end
 
+
+  def viewMigraingeDayAnalytics
+    daysOfWeek = {0 => "Sunday",1 => "Monday",2 => "Tuesday",3 => "Wednesday",4 => "Thursday",5 => "Friday",
+      6 => "Saturday"}
+    days = {"Sunday" => 0,"Monday" => 0,"Tuesday" => 0,"Wednesday" => 0,"Thursday" => 0,"Friday" => 0,"Saturday" => 0}
+    if current_user!=nil 
+      migraine_reports = current_user.migraine_reports
+      migraine_reports.each do |report|
+       days[daysOfWeek[report.start.wday]] += 1
+      end
+    end
+    max = 0
+    maxDay = ""
+    days.each do |day, count| 
+      if count > max
+        max = count
+        maxDay = day
+      end
+    end
+    @days = days
+    @maxDay = maxDay
+    respond_to do |format|
+       format.html
+    end
+  end
 end
